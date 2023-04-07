1a705146e52adbece7091a5f00e6c407c1cd3eaf
Juan José López Díaz <juancorvelle@gmail.com>
2023-04-07 19:09:10 +0200

    create store

---
+++ store/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InicioComponent,
+    NavComponent,
+    FooterComponent,
+    LoginComponent
+  ],
+  imports: [
+
+  BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
